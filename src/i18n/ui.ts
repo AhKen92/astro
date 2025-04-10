290f8a4aba9a74ec40fd01818345032e8a17ba77
AhKen92 <118493553+AhKen92@users.noreply.github.com>
2025-04-10 18:55:40 +0800

    change language

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,8 +1,8 @@
 export const ui = {
   'zh': {
-    title: '重新编排',
-    subtitle: '再现版式之美',
-    description: 'Retypeset是一款基于Astro框架的静态博客主题，中文名为重新编排。本主题以活版印字为设计灵感，通过建立全新的视觉规范，对所有页面进行重新编排，打造纸质书页般的阅读体验，再现版式之美。所见皆为细节，方寸尽显优雅。',
+    title: 'Ah Ken的频道',
+    subtitle: '种一棵树最好的时间是十年前，其次是现在',
+    description: '暂时还没想好',
     posts: '文章',
     tags: '标签',
     about: '关于',
